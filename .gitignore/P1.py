bc93d6adc11a3c3229cbf1811e5e0e48d6bfea3b
GUZIRAN <ziran.gu@gmail.com>
2017-06-22 17:41:12 -0500

    Update P1.py

--- .gitignore/P1.py
+++ .gitignore/P1.py
@@ -4,8 +4,7 @@ import numpy as np
 import seaborn as ssn
 
 
-from pyheatmap.heatmap import HeatMap
-data = pd.read_csv('Data.csv')
+data = pd.read_table('REG.TXT')
 header = list(data.columns)
 
 for i in xrange(len(header)):
@@ -16,21 +15,23 @@ gene = ct.keys()
 Tdata = data.T
 Tdata['Gene'] = header
 DataList = []
+smax = np.nanmax(data.values.astype(np.float64))
+smin = np.nanmin(data.values.astype(np.float64))
 
 for i in xrange(len(gene)):
 	DataList.append(Tdata.loc[Tdata.Gene == gene[i]])
 
+plotlist=[]
 for i in xrange(len(gene)):
 	DataList[i]=DataList[i].drop('Gene',1).T
 	DataList[i].index = range(len(DataList[i]))
-	print gene[i] + 'file shape :' + str (DataList[i].shape)
-	
+	#print gene[i] + 'file shape :' + str (DataList[i].shape)
+	plotlist.append(plt.subplot(1,8,i+1))
+	ssn.heatmap(DataList[i], cbar = False, xticklabels = False, yticklabels = False,vmin = smin,vmax = smax)
 
+plt.savefig('Heatmap.png')
 
+#now plot the heatmap:
 #plt.pcolor(DataList[0])
 #plt.yticks(np.arange(0.5, len(DataList[0].index), 1), DataList[0].index)
 #plt.xticks(np.arange(0.5, len(DataList[0].columns), 1), DataList[0].columns)
-d1=DataList[0]
-d1h=HeatMap(d1)
-
-
